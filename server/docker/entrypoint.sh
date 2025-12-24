992fcab8bf5896dd33d873586cbeb02a82c7ae99
otsairaze <ssavelevdenchikk@gmail.com>
2025-12-24 21:28:58 +0100

    ci: create vercel auto deploy pipeline

--- server/docker/entrypoint.sh
+++ server/docker/entrypoint.sh
@@ -1,11 +1,1 @@
-#!/bin/sh
-set -e
-
-echo "[DEBUG] Waiting for postgres (sleeping 5s)..."
-sleep 5
-
-echo "[DEBUG] Start prisma migration"
-npx prisma migrate deploy
-
-echo "[DEBUG] Starting application..."
-exec node dist/src/main.js
+ 
